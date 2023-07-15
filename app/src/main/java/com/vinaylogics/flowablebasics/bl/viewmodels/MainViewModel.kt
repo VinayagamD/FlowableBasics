a66996ae157494db518c9809b24e1e5ec2b7703b
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-15 10:47:06 +0530

    Terminal flow operator reduce

--- app/src/main/java/com/vinaylogics/flowablebasics/bl/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/vinaylogics/flowablebasics/bl/viewmodels/MainViewModel.kt
@@ -3,21 +3,15 @@ package com.vinaylogics.flowablebasics.bl.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.reduce
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
 class MainViewModel : ViewModel() {
 
     val countDownFlow = flow<Int> {
-        val startingValue = 10
+        val startingValue = 5
         var currentValue = startingValue
         emit(startingValue)
         while (currentValue > 0) {
@@ -33,19 +27,11 @@ class MainViewModel : ViewModel() {
 
     private fun collectFlow() {
         viewModelScope.launch {
-            val count = countDownFlow
-                .filter { time ->
-                    time % 2 == 0
+            val reduceResult = countDownFlow
+                .reduce { accumulator, value ->
+                    accumulator + value
                 }
-                .map { time ->
-                    time * time
-                }.onEach {time ->
-                    println(time)
-                }
-                .count {
-                    it % 2 ==0
-                }
-            println("Count is $count")
+            println("Reduce result is $reduceResult")
         }
     }
 }
